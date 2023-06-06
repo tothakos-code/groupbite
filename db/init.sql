88aea74fcd8ef7a089a664e8d06d84f66ebdbe06
Tóth Ákos <takos2070@gmail.com>
2023-06-06 21:43:49 +0200

    date db column unique

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,7 @@
 CREATE TYPE order_state AS ENUM ('collect', 'order', 'closed');
 CREATE TABLE if not exists orders (
   id SERIAL PRIMARY KEY,
-  order_date DATE DEFAULT CURRENT_DATE,
+  order_date DATE UNIQUE DEFAULT CURRENT_DATE,
   order_state order_state DEFAULT 'collect',
   basket jsonb
 );

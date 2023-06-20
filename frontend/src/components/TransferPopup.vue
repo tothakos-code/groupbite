0f6d8e2c95d830906b2ab0b4a70d91fbb31f4c3e
Tóth Ákos <takos2070@gmail.com>
2023-06-20 20:47:02 +0200

    Adding Example order description with copy to clipboard button

--- frontend/src/components/TransferPopup.vue
+++ frontend/src/components/TransferPopup.vue
@@ -27,6 +27,20 @@
   >
     <p>Kosár sikeresen átmásolva a falusira. Frissítsd a falusi oldalát.</p>
     <p>
+    Rendelés megjegyzés példa:
+    </p>
+    <div class="d-flex">
+      <div class="border bg-light-subtle rounded ps-2">
+        <code>{{ this.orderDesc }}</code>
+      </div>
+      <button type="button" name="button" title="Copy to clipboard" class="btn " @click="this.doCopy()">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-clipboard2" viewBox="0 0 16 16">
+          <path d="M3.5 2a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-12a.5.5 0 0 0-.5-.5H12a.5.5 0 0 1 0-1h.5A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1H4a.5.5 0 0 1 0 1h-.5Z"/>
+          <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z"/>
+        </svg>
+      </button>
+    </div>
+    <br>
     <p>Okézd le , ha kifizetted a rendelést</p>
   </Popup>
 </template>
@@ -34,6 +48,7 @@
 <script>
 import Popup from './Popup.vue';
 import { state, socket } from "@/socket";
+import { copyText } from 'vue3-clipboard';
 
 export default {
   name: 'TransferPopup',
@@ -45,6 +60,7 @@ export default {
       showInitial: false,
       showSpinner: false,
       showFinish: true,
+      orderDesc: "Tigra kft. Csere edényeket adunk. Mindent külön edénybe szeretnénk. Nagyon szépen köszönjük.",
       psid: ""
     }
   },
@@ -84,6 +100,17 @@ export default {
     orderPayed: function() {
       socket.emit("Ordered and Payed");
       this.showFinish = false;
+    },
+    doCopy: function() {
+      copyText(this.orderDesc, undefined, (error, event) =>{
+        if (error) {
+           alert('Can not copy')
+           console.log(error)
+         } else {
+           alert('Copied')
+           console.log(event)
+         }
+      });
     }
   }
 }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import VueCookies from 'vue3-cookies';
+import VueClipboard from 'vue3-clipboard';
 
 let app = createApp(App);
 app.use(VueCookies);
+app.use(VueClipboard, {
+  autoSetContainer: true,
+  appendToBody: true,
+});
 app.mount('#app');

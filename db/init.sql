f94f5ada1a7a8deb6ca8edb6ab52404f1bbb1097
Tóth Ákos <takos2070@gmail.com>
2023-06-25 17:04:31 +0200

    New database table for users

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,17 @@
-CREATE TYPE order_state AS ENUM ('collect', 'order', 'closed');
+CREATE TYPE order_state_type AS ENUM ('collect', 'order', 'closed');
+CREATE TYPE subscribe_type AS ENUM ('none', 'temp', 'full');
+CREATE TYPE daily_state_type AS ENUM ('none', 'video', 'skip', 'done');
+
 CREATE TABLE if not exists orders (
   id SERIAL PRIMARY KEY,
   order_date DATE UNIQUE DEFAULT CURRENT_DATE,
-  order_state order_state DEFAULT 'collect',
+  order_state order_state_type DEFAULT 'collect',
   basket jsonb
+);
+
+CREATE TABLE if not exists users (
+  id SERIAL PRIMARY KEY,
+  username text UNIQUE NOT NULL,
+  subscribed subscribe_type NOT NULL DEFAULT 'none',
+  daily_state daily_state_type NOT NULL DEFAULT 'none'
 );

f15c64ba7e46939a7147fde6eb1ab3b18a9bdc4d
Tóth Ákos <takos2070@gmail.com>
2023-06-25 17:02:50 +0200

    Show empty basket + basket value

--- frontend/src/components/LocalBasket.vue
+++ frontend/src/components/LocalBasket.vue
@@ -5,7 +5,7 @@
         <h2>Kosarad</h2>
       </div>
       <div class="col-4 align-items-center">
-        <span id="localbasket-value"></span>
+        <span>{{ sumBasket }} Ft + Szállítási díj</span>
       </div>
       <div class="col-4 text-end">
         <button class="btn btn-warning" @click="this.clearBasket()">Kosarad ürítése
@@ -17,7 +17,7 @@
       </div>
     </div>
     <div class="row">
-      <div class="list-group pe-0" id="basketList">
+      <div class="list-group pe-0">
         <div v-for="(item, itemSizeKey) in basket" :key="itemSizeKey" class="list-group-item d-flex justify-content-between">
           <div class="col-2">
             <span class="badge bg-warning-subtle border border-warning-subtle text-warning-emphasis rounded-pill">{{  item.quantity }} x</span>
@@ -27,6 +27,17 @@
           </div>
           <div class="col-2 text-end">
             <button @click="this.deleteFromBasket(itemSizeKey, item)" type="button" title="Törlés" class="btn btn-close"></button>
+          </div>
+        </div>
+        <div v-if="isBasketEmpty" class="list-group-item d-flex justify-content-center">
+          <div class="">
+            <span>
+              Üres a kosarad
+            </span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-cart-x" viewBox="0 0 16 16">
+              <path d="M7.354 5.646a.5.5 0 1 0-.708.708L7.793 7.5 6.646 8.646a.5.5 0 1 0 .708.708L8.5 8.207l1.146 1.147a.5.5 0 0 0 .708-.708L9.207 7.5l1.147-1.146a.5.5 0 0 0-.708-.708L8.5 6.793 7.354 5.646z"/>
+              <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+            </svg>
           </div>
         </div>
       </div>
@@ -84,6 +95,18 @@ export default {
       this.cookies.set('basket', {}, '16h');
       this.$emit('basketUpdate');
       this.updateBasket();
+    }
+  },
+  computed: {
+    isBasketEmpty() {
+      return Object.keys(this.basket).length == 0;
+    },
+    sumBasket() {
+      let sum = 0;
+      Object.values(this.basket).forEach(item => {
+        sum+= Number(item.quantity) * Number((item.price).split(' ')[0]);
+      });
+      return sum;
     }
   }
 }

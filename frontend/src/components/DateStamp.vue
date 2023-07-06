d90e35b6a4c321814cd79dc52e71fb43bc0abec6
Tóth Ákos <takos2070@gmail.com>
2023-07-06 21:01:38 +0200

    Nicer dateselector layout

--- frontend/src/components/DateStamp.vue
+++ frontend/src/components/DateStamp.vue
@@ -1,28 +1,30 @@
 <template>
-  <div class="row d-flex justify-content-between">
-    <div class="col row d-flex justify-content-center align-items-center">
-      <div class="col text-end">
-        <button type="button" name="button" class="btn btn-link link-warning" :disabled="this.limitToCurrentWeek && this.currentDateSelected.getDay() == 1" @click="this.prevDay()">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-left-square" viewBox="0 0 16 16">
-            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-            <path d="M10.205 12.456A.5.5 0 0 0 10.5 12V4a.5.5 0 0 0-.832-.374l-4.5 4a.5.5 0 0 0 0 .748l4.5 4a.5.5 0 0 0 .537.082z"/>
-          </svg>
-        </button>
-      </div>
-      <div class="col">
-        <span class="row justify-content-center">{{ getTodayDayDate }}</span>
-        <span class="row justify-content-center">{{ getTodayDayName }}</span>
-      </div>
-      <div class="col text-start">
-        <button type="button" name="button" class="btn btn-link link-warning" :disabled="this.limitToCurrentWeek && this.currentDateSelected.getDay() == 0" @click="this.nextDay()">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-right-square" viewBox="0 0 16 16">
-            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-            <path d="M5.795 12.456A.5.5 0 0 1 5.5 12V4a.5.5 0 0 1 .832-.374l4.5 4a.5.5 0 0 1 0 .748l-4.5 4a.5.5 0 0 1-.537.082z"/>
-          </svg>
-        </button>
+  <div class="row d-flex justify-content-end">
+    <div class="col my-auto">
+      <div class="row d-flex justify-content-center align-items-center">
+        <div class="col text-end">
+          <button type="button" name="button" class="btn btn-link link-warning" :disabled="this.limitToCurrentWeek && this.currentDateSelected.getDay() == 1" @click="this.prevDay()">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-left-square" viewBox="0 0 16 16">
+              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+              <path d="M10.205 12.456A.5.5 0 0 0 10.5 12V4a.5.5 0 0 0-.832-.374l-4.5 4a.5.5 0 0 0 0 .748l4.5 4a.5.5 0 0 0 .537.082z"/>
+            </svg>
+          </button>
+        </div>
+        <div class="col">
+          <span class="row justify-content-center">{{ getTodayDayDate }}</span>
+          <span class="row justify-content-center">{{ getTodayDayName }}</span>
+        </div>
+        <div class="col text-start">
+          <button type="button" name="button" class="btn btn-link link-warning" :disabled="this.limitToCurrentWeek && this.currentDateSelected.getDay() == 0" @click="this.nextDay()">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-right-square" viewBox="0 0 16 16">
+              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+              <path d="M5.795 12.456A.5.5 0 0 1 5.5 12V4a.5.5 0 0 1 .832-.374l4.5 4a.5.5 0 0 1 0 .748l-4.5 4a.5.5 0 0 1-.537.082z"/>
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
-    <div class="col my-auto">
+    <div class="col-2 my-auto">
       <button v-if="this.currentDate.getDate() !== this.currentDateSelected.getDate()" type="button" name="button" @click="this.jumpToPresentDay()" class="btn btn-warning">
         <span>Ma</span>
       </button>

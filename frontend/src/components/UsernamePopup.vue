999df5011ebd978c8e95383b46186f1d4b9754f7
Tóth Ákos <takos2070@gmail.com>
2023-07-08 11:39:24 +0200

    Pointer fix on hover

--- frontend/src/components/UsernamePopup.vue
+++ frontend/src/components/UsernamePopup.vue
@@ -5,17 +5,17 @@
       <button type="button" class="btn btn-outline-dark dropdown-toggle dropdown-toggle-split" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" ></button>
       <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
         <li>
-          <a class="dropdown-item dropdown-menu-lg-end" @click="showProfile = true">
+          <button class="dropdown-item" @click="showProfile = true">
             <span>
               Név szerkesztése
             </span>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;">
               <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"></path>
             </svg>
-          </a>
+          </button>
         </li>
         <li>
-          <a class="dropdown-item" @click="this.$emit('toHistory')">
+          <button class="dropdown-item" @click="this.$emit('toHistory')">
             <span>
               Előző rendelések
             </span>
@@ -24,7 +24,7 @@
               <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0v1z"/>
               <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"/>
             </svg>
-          </a>
+          </button>
         </li>
       </ul>
     </div>

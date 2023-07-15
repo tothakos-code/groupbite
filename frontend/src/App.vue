7383a928a8422adc45959a7e043e1379bab3f3b1
Tóth Ákos <takos2070@gmail.com>
2023-07-15 11:35:24 +0200

    Fix switched lines

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,9 +11,8 @@
           <button v-else class="btn btn-light ms-2" @click="this.toggleDarkMode()">Light</button>
         </div>
         <div v-if="isLoggedIn" class="d-flex justify-content-start">
-
-            <button v-if="this.subscriptionState != 'full'" type="button" class="btn btn-warning mx-2 position-relative" @click="this.subscribe()">
           <div class="" title="Ha feliratkozol megjelenik a neved minden nap a közös kosárban. Így a többiek látják, hogy még nem választottál és biztos nem maradsz le a rendelésről.">
+            <button v-if="this.subscriptionState != 'full'" type="button" class="btn btn-warning mx-2 position-relative" @click="this.subscribe()">
               Feliratkozás
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
                 <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
@@ -213,6 +212,7 @@ export default {
   }
 }
 </script>
+
 
 <style>
 </style>

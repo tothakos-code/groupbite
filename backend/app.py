402d31e918770efe02f4d104689f0f3f52a5cc12
Tóth Ákos <takos2070@gmail.com>
2023-07-27 10:06:17 +0200

    Migrating basket handling

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,6 @@ from flask import request
 import requests,json,re
 from bs4 import BeautifulSoup
 from flask_socketio import SocketIO
-from collections import Counter
 import logging
 import datetime
 import db
@@ -14,22 +13,11 @@ socketio = SocketIO(app,logger=True, engineio_logger=True)
 app.config['SECRET_KEY'] = 'secret!'
 
 from controllers.menu_controller import menu_controller
-from controllers.order_controller import order_controller
+from controllers.order_controller import order_controller, get_today_basket
 from controllers.user_controller import user_controller, emit_user_ds_state
 app.register_blueprint(menu_controller)
 app.register_blueprint(order_controller)
 app.register_blueprint(user_controller)
-
-sidfdpattern = r"\/sidfd-[0-9]+\/"
-
-sql_select = "SELECT basket FROM orders WHERE order_date = CURRENT_DATE"
-sql_get_state = "SELECT order_state FROM orders WHERE order_date = CURRENT_DATE"
-sql_set_state = "UPDATE orders SET order_state = %s WHERE order_date = CURRENT_DATE RETURNING order_state"
-sql_insert = "INSERT INTO orders (basket) values (%s) RETURNING basket"
-sql_update = "UPDATE orders SET basket = %s WHERE order_date = CURRENT_DATE RETURNING basket"
-
-
-sql_order_select_by_date = "SELECT basket FROM orders WHERE order_date = %s"
 
 
 @app.route("/cron/new_day_refresh")
@@ -46,139 +34,12 @@ def call_clear_clients_basket():
     socketio.emit('Clear Local Basket')
     return "OK"
 
-@app.route('/transferBasket', methods=['POST'])
-def call_transfer_basket():
-    PHPSESSIONID = request.json['psid']
-    orders = None
-
-    orders = db.run_sql(sql_select, fetch='one')[0]
-
-    db.run_sql(sql_set_state, ('order',))
-    logging.info("Order status changet to 'order'")
-
-    socketio.emit("Order state changed", "order")
-
-    if orders is None:
-        logging.warning("Error: basket empty or could not get basket from database.")
-        return "Error: basket empty or could not get basket from database."
-
-    # Create the list of item links
-    orderList = []
-    for person, basket in orders.items():
-        # ToDo: prevent None
-        if basket == None:
-            continue
-        for foodItem in basket.values():
-            for quantity in range(0,foodItem['quantity']):
-                orderList.append(foodItem['link'])
-
-
-    requests_header={
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Connection": "keep-alive",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept": "*/*",
-        "Host": "falusitekercsgyorsetterem.pgg.hu"
-        }
-
-    # Loop the links and make the requests
-    logging.info(Counter(orderList))
-    for link,count in Counter(orderList).items():
-        sizeid = re.search(sidfdpattern, link).group().replace('/','').split('-')[1]
-
-        requests_data={
-            "item_cnt": count,
-            "item_size_ref": sizeid,
-            "plusmenu[]": "112",
-            "menu_a[]": "0",
-            "plus0[]": "112",
-            "act": "add",
-            "compAll": ""
-            }
-
-        r = requests.post(
-            link,
-            headers=requests_header,
-            data=requests_data,
-            cookies={"PHPSESSID":PHPSESSIONID},
-            verify=False
-        )
-    return 'OK'
 
 @socketio.on('connect')
 def handle_connect(data):
     socketio.emit('Client Basket Update', {'basket': get_today_basket() })
     emit_user_ds_state()
 
-@socketio.on('Ordered and Payed')
-def handle_payed():
-    db.run_sql(sql_set_state, ('closed',))
-    socketio.emit("Order state changed", "closed")
-
-@socketio.on('Server Basket Update')
-def handle_basket_update(data):
-    order_state = get_order_state()
-    if order_state == "closed":
-        socketio.emit('Client Basket Update', {'basket': get_today_basket() })
-        socketio.emit("Order state changed", order_state, room=request.sid)
-        return
-    user = list(data.keys())[0]
-    currentBasket = get_today_basket()
-    if data[user]:
-        # basket is not empty, save new basket
-        currentBasket[user] = data[user]
-    elif user in currentBasket:
-        # basket is empty, delete key
-        del currentBasket[user]
-
-    socketio.emit('Client Basket Update', {'basket': set_today_basket(currentBasket) })
-
-def get_order_state():
-    rowcount = db.get_row_count(sql_select)
-    result = db.run_sql(sql_get_state, fetch='one')
-
-    if rowcount == 0:
-        # orderer not initialized yet, return the default value
-        return 'collect'
-    elif rowcount == 1:
-        # return the state
-        return result
-    elif rowcount > 1:
-        # this error is impossible
-        logging.error("ERROR: There is more than one order, I dont know what to do! PANIC!")
-
-
-def get_today_basket():
-    rowcount = db.get_row_count(sql_select)
-
-    if rowcount == 0:
-        # return empty basket, there is not yet created
-        return {}
-    elif rowcount == 1:
-        # return the basket
-        return db.run_sql(sql_select, fetch='one')[0]
-    elif rowcount > 1:
-        # this error is impossible
-        logging.error("ERROR: There is more than one order, I dont know what to do! PANIC!")
-
-def set_today_basket(basket):
-    rowcount = db.get_row_count(sql_select)
-
-    result = None
-
-    if rowcount == 0:
-        # insert row
-        result = db.run_sql(sql_insert, (json.dumps(basket),), fetch='one')[0]
-        logging.info("Created today's basket row")
-    elif rowcount == 1:
-        # update row
-        result = db.run_sql(sql_update, (json.dumps(basket),), fetch='one')[0]
-        logging.info("Updated today's basket row")
-    elif rowcount > 1:
-        # this is impossible
-        logging.error("ERROR: There is more than one order, I dont know what to do! PANIC!")
-
-    return result
 
 if __name__ == "__main__":
     socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
--- backend/controllers/order_controller.py
+++ backend/controllers/order_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 from entities.order import Order, OrderSchema, order_state_type
+from collections import Counter
 
 import requests, json, re
 import logging
@@ -7,6 +8,8 @@ from datetime import date, timedelta, datetime
 from sqlalchemy import func, cast
 from entities.entity import Session
 from __main__ import socketio
+
+sidfdpattern = r"\/sidfd-[0-9]+\/"
 
 order_controller = Blueprint('order_controller', __name__, url_prefix='/order')
 
@@ -20,12 +23,79 @@ def handle_order_history(requested_date):
         return {}
     return order_history.serialized['basket']
 
+
 @order_controller.route('/get-order-state')
 def handle_request_order_state():
     return json.dumps({"order_state":get_order_state()})
 
 
+@socketio.on('Server Basket Update')
+def handle_basket_update(data):
+    order_state = get_order_state()
+    if order_state == str(order_state_type.closed):
+        socketio.emit('Client Basket Update', {'basket': get_today_basket() })
+        socketio.emit("Order state changed", order_state, room=request.sid)
+        return
+    user = list(data.keys())[0]
+    currentBasket = get_today_basket()
+    if data[user]:
+        # basket is not empty, save new basket
+        currentBasket[user] = data[user]
+    elif user in currentBasket:
+        # basket is empty, delete key
+        del currentBasket[user]
+
+    socketio.emit('Client Basket Update', {'basket': set_today_basket(currentBasket) })
+
+
+def get_today_basket():
+    session = Session()
+    today_basket = session.query(Order).filter(Order.order_date == date.today().strftime('%Y-%m-%d')).first()
+    session.close()
+
+    if not today_basket:
+        return {}
+    return today_basket.basket
+
+
+def set_today_basket(basket):
+    session = Session()
+    today_basket = session.query(Order).filter(Order.order_date == date.today().strftime('%Y-%m-%d')).first()
+
+    if not today_basket:
+        # insert
+        session.add(Order(date.today().strftime('%Y-%m-%d'), basket))
+        logging.info("Created today's basket row")
+    else:
+        # update
+        today_basket.basket = basket
+        logging.info("Updated today's basket row")
+
+    session.commit()
+
+    return get_today_basket()
+
+
+@socketio.on('Ordered and Payed')
+def handle_payed():
+    session = Session()
+    order_state = session.query(Order).filter(Order.order_date == date.today().strftime('%Y-%m-%d')).first()
+    order_state.order_state = order_state_type.closed
+    session.commit()
+    socketio.emit("Order state changed", str(order_state_type.closed))
+
+
 def get_order_state():
+    """Returns to current order state value
+    Parameters
+    ----------
+    none
+
+    Returns
+    -------
+    String
+        A order_state_type value.
+    """
     session = Session()
     order_state = session.query(Order).filter(Order.order_date == date.today().strftime('%Y-%m-%d')).first()
     session.close()
@@ -33,3 +103,82 @@ def get_order_state():
         return str(order_state_type.collect)
 
     return str(order_state.order_state)
+
+
+def set_order_state(new_state):
+    """Setter for today's order state
+    Parameters
+    ----------
+    new_state: An order_state_type object to sot the order_state to.
+
+    Returns
+    -------
+    order_state_type
+        An order_state_type object.
+    """
+    session = Session()
+    today_order = session.query(Order).filter(Order.order_date == date.today().strftime('%Y-%m-%d')).first()
+    if not today_order:
+        session.close()
+        return str(order_state_type.collect)
+
+    today_order.order_state = new_state
+    session.commit()
+    return str(today_order.order_state)
+
+
+@order_controller.route('/transferBasket', methods=['POST'])
+def call_transfer_basket():
+    PHPSESSIONID = request.json['psid']
+
+    orders = get_today_basket()
+
+    set_order_state(order_state_type.order)
+    logging.info("Order status changet to 'order'")
+    socketio.emit("Order state changed", "order")
+
+
+    if orders == {}:
+        logging.warning("Error: basket is empty or could not get basket from database.")
+        return "Error: basket empty or could not get basket from database."
+
+    # Create the list of item links
+    orderList = []
+    for person, basket in orders.items():
+        # ToDo: prevent None
+        if basket == None:
+            continue
+        for foodItem in basket.values():
+            for quantity in range(0,foodItem['quantity']):
+                orderList.append(foodItem['link'])
+
+
+    requests_header={
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Connection": "keep-alive",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept": "*/*",
+        "Host": "falusitekercsgyorsetterem.pgg.hu"
+        }
+
+    # Loop the links and make the requests
+    for link,count in Counter(orderList).items():
+        sizeid = re.search(sidfdpattern, link).group().replace('/','').split('-')[1]
+
+        requests_data={
+            "item_cnt": count,
+            "item_size_ref": sizeid,
+            "plusmenu[]": "112",
+            "menu_a[]": "0",
+            "plus0[]": "112",
+            "act": "add",
+            "compAll": ""
+            }
+
+        # r = requests.post(
+        #     link,
+        #     headers=requests_header,
+        #     data=requests_data,
+        #     cookies={"PHPSESSID":PHPSESSIONID}
+        # )
+    return 'OK'

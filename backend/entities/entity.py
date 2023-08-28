75f27c9d1ba6484b6fdf2b46f6e99cce7ddb7539
Tóth Ákos <takos2070@gmail.com>
2023-08-28 20:10:43 +0200

    Increasing pool size

--- backend/entities/entity.py
+++ backend/entities/entity.py
@@ -7,7 +7,7 @@ db_url = 'database:5432'
 db_name = 'falusi'
 db_user = 'falusi'
 db_password = 'falusi'
-engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}')
+engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}',pool_size=10, max_overflow=20)
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()

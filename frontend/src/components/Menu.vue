b9ef40708ca4e0266f73c2023ed9f62df903a203
Tóth Ákos <takos2070@gmail.com>
2023-11-05 20:11:04 +0100

    Menus for all days

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -2,25 +2,58 @@
   <div class="card">
     <div class="card-header row d-flex px-2">
       <div class="col-3 col-md-6 col-lg-8 row px-0">
-        <div class="col-12 col-lg-4 my-auto">
-          <h2 class="">Étlap</h2>
+        <div class="col-0 d-none d-sm-inline col-sm-6 col-lg-4 my-auto">
+          <h2 class="text-nowrap">Étlap</h2>
         </div>
         <div class="col-0 d-none col-lg-8 d-lg-inline my-auto truncate">
           <TransferPopup/>
         </div>
       </div>
-      <div class="col-9 col-md-6 col-lg-4 d-flex flex-fill px-0">
-        <Datestamp @selectedDate="(day) => this.getMenu(day)" :limitToCurrentWeek="true"/>
+      <div class="col-12 col-sm-6 col-md-6 col-lg-4 d-flex flex-fill ">
+        <Datestamp
+          ref="dateSelector"
+          @selectedDate="(day) => this.getMenu(day)"
+          :limitToCurrentWeek="true"
+        />
       </div>
+    </div>
+    <div class="row d-flex justify-content-evenly">
+      <div
+        v-for="date,index in getCurrentWeekDates()"
+        class="d-flex justify-content-center flex-fill col-6 col-sm-4 col-lg-3 col-xxl-1"
+        :key="index"
+      >
+        <input
+          type="radio"
+          name="daySelectionRadio"
+          :id="date"
+          class="btn-check"
+          role="button"
+          @click="loadMenu(date)"
+          :checked="date == new Date(selectedDate).toISOString().split('T')[0]"
+        >
+        <label
+          :for="date"
+          class="btn btn-sm my-1 rounded rounded-5 text-nowrap d-flex align-items-center"
+          :class="['btn-outline-' + auth.userColor.value]"
+        >
+          {{ dayTitles[index] }}
+        </label>
+      </div>
+
     </div>
     <div class="row col list-group">
       <div class="col">
-        <div class="list-group my-1">
+        <div class="list-group m-1">
           <MenuItem
-            v-for="item in items"
-            :key="item.id"
-            :item="item">
+          v-for="item in menulist"
+          :key="item.id"
+          :item="item"
+          >
           </MenuItem>
+          <div class="d-flex justify-content-center" v-if="menulist.length === 0">
+            <span>Erre a napra nincsen mit betöltenem</span>
+          </div>
         </div>
       </div>
     </div>
@@ -30,7 +63,7 @@
 <script>
 import Datestamp from './DateStamp.vue'
 import MenuItem from './MenuItem.vue'
-import { state } from "@/socket";
+import { state, socket } from "@/socket";
 import { useAuth } from "@/auth";
 import TransferPopup from './TransferPopup.vue'
 
@@ -53,28 +86,73 @@ export default {
     this.getMenu();
   },
   methods: {
-    getMenu: function(day) {
-      let url = ''
+    loadMenu(day) {
       if (day === undefined) {
-        url = `http://${window.location.host}/api/menu/get/${new Date().toISOString().split('T')[0]}`
-      } else {
-        url = `http://${window.location.host}/api/menu/get/${new Date(day).toISOString().split('T')[0]}`
+        day = new Date().toISOString().split('T')[0]
       }
+      let url = `http://${window.location.host}/api/menu/get/${day}`
 
       fetch(url)
         .then(response => response.json())
           .then(data => {
-            this.items = data;
+            this.menulist = data;
           })
         .catch(error => console.error(error));
+
+    },
+    getMenu: function(day) {
+      if (day === undefined) {
+        day = new Date()
+        if (day.getHours() >= 13) {
+          day.setDate(day.getDate() +1)
+          this.$refs.dateSelector.setDay(day)
+        }
+      }
+      let formated_day = new Date(day).toISOString().split('T')[0];
+      socket.emit("Client Date Selection Change", {"old_selected_date": state.selectedDate.toISOString().split('T')[0] ,"selected_date": formated_day})
+      state.selectedDate = day;
+      this.loadMenu(formated_day);
+    },
+    getCurrentWeekDates() {
+      const currentDate = new Date();
+      const currentDayOfWeek = currentDate.getDay();
+
+      // Calculate the start date of the current week (Sunday)
+      const startDate = new Date(currentDate);
+      startDate.setDate(currentDate.getDate() - currentDayOfWeek + (currentDayOfWeek === 0 ? -6 : 1));
+
+      const weekDates = [];
+
+      // Loop through the days of the week and format the dates
+      for (let i = 0; i < 7; i++) {
+        const currentDate = new Date(startDate);
+        currentDate.setDate(startDate.getDate() + i);
+
+        const formattedDate = currentDate.toISOString().split('T')[0];
+        weekDates.push(formattedDate);
+      }
+
+      return weekDates;
     }
   },
   data() {
     return {
-      items: []
+      menulist: [],
+      others: {'0':'[]','1':'[]','2':'[]','3':'[]','4':'[]','5':'[]','6':'[]'},
+      dayTitles: {
+        '0':'Hétfői menü',
+        '1':'Keddi  menü',
+        '2':'Szerdai menü',
+        '3':'Csütörtöki menü',
+        '4':'Pénteki menü',
+        '5':'Szombati menü',
+        '6':'Vasárnapi menü'
+      }
     }
   },
   computed: {
+    selectedDate() {
+      return state.selectedDate;
     }
   }
 }

129d108c3b3bf9229df5a4b299e723537973e034
Tóth Ákos <takos2070@gmail.com>
2023-11-06 18:31:43 +0100

    Copy button color fix

--- frontend/src/components/GlobalBasketPerson.vue
+++ frontend/src/components/GlobalBasketPerson.vue
@@ -3,7 +3,7 @@
       <div class="card-header row d-flex pe-0">
         <div class="col-6">
           <span>{{ name }}</span>
-          <a v-if="copyable && name !== loggedInUser" class="ms-2 p-1 btn btn-sm" :class="['btn-' + auth.userColor ]" @click="copy">
+          <a v-if="copyable && name !== loggedInUser" class="ms-2 p-1 btn btn-sm" :class="['btn-' + auth.userColor.value ]" @click="copy">
             Másol
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard2" viewBox="0 0 16 16">
               <path d="M3.5 2a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-12a.5.5 0 0 0-.5-.5H12a.5.5 0 0 1 0-1h.5A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1H4a.5.5 0 0 1 0 1h-.5Z"/>

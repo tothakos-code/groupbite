900127047723021dd441edb479d604c22f7550f8
Tóth Ákos <takos2070@gmail.com>
2024-03-05 20:23:57 +0100

    Route loading, not final

--- frontend/src/loader.js
+++ frontend/src/loader.js
@@ -1,3 +1,4 @@
+import { state } from "@/socket";
 
 export function register_plugin_routes(router) {
   fetch(`http://${window.location.host}/api/vendor/find-all-active`,{
@@ -9,13 +10,25 @@ export function register_plugin_routes(router) {
           router.addRoute({
             name: item.name,
             path: '/'+item.name,
+            redirect: () => {
+              let day = state.selectedDate
+              if (day.getHours() >= 13) {
+                day.setDate(day.getDate() +1)
+              }
+              return {"path":`/${item.name}/`+day.toISODate()};
+            }
+          });
+          // router.replace();
+          router.addRoute({
+            name: item.name+'-dated',
+            path: '/'+item.name+'/:selected_date',
             component: () => import(`@/../../plugins/${item.name}/frontend/App.vue`),
-          })
+          });
           console.log('registering:');
           console.log(item);
         });
-        router.replace();
-        // router.replace(router.currentRoute.value.fullPath)
+        const currentRoute = router.resolve(router.currentRoute.value.path);
+        router.replace(currentRoute);
       })
     .catch(error => console.error('Error fecthing vendors:',error));
 }

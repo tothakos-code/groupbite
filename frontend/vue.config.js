78f015533d9694c76efdee76f270d21793da0f27
Tóth Ákos <takos2070@gmail.com>
2024-06-30 09:47:27 +0200

    Dead code

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,7 +7,6 @@ module.exports = defineConfig({
     // Set environment variables for Webpack
     config.plugin('define').tap((args) => {
       args[0]['process.env'].VUE_APP_VERSION = JSON.stringify(packageJson.version);
-      args[0]['process.env'].VUE_APP_FALU_BANNER = JSON.stringify(process.env.FALU_BANNER || '');
       return args;
     });
   },
